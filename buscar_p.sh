9957e556b613314b2bb557249cbe32bad0b54369
asam1996 <118818894+asam1996@users.noreply.github.com>
2023-04-10 23:28:52 +0200

    Update buscar_p.sh

--- buscar_p.sh
+++ buscar_p.sh
@@ -2,4 +2,8 @@
 #this is a comment marked as the change c in this project
 
 r='[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
-i=$(ip address)
+echo $(ip address) > ipp
+for i in $(cat ipp); do
+  echo "${i}"
+done
+rm ipp
